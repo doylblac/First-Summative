bd45517a9a263a87d20733c6441cb789ba921676
DoyleandEmily <DoyleandEmily@DOYLE>
2016-09-21 13:34:35 -0400

    First Commit

--- First Summative/Form1.Designer.cs
+++ First Summative/Form1.Designer.cs
@@ -28,15 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::First_Summative.Properties.Resources.phoenix;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Greeting Card";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
 
--- First Summative/Form1.cs
+++ First Summative/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace First_Summative
 {
@@ -14,15 +15,43 @@ namespace First_Summative
     {
         public Form1()
         {
+           // this.BackgroundImage = Properties.Resources.phoenix;
+
             InitializeComponent();
         }
 
         private void Form1_Click(object sender, EventArgs e)
         {
-            BackColor = Color.Yellow;
+            Font drawFont = new Font("Arial", 30, FontStyle.Bold);
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush BlackBrush = new SolidBrush(Color.Black);
+
+            formGraphics.Clear(Color.White);
+
+            formGraphics.DrawString("Hello", drawFont, BlackBrush, 50, 40);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            Font drawFont = new Font("Arial", 30, FontStyle.Bold);
+            SolidBrush BlackBrush = new SolidBrush(Color.Purple);
+
+            this.Size = new Size(450, 650);
+
+            BackgroundImageLayout = ImageLayout.Stretch;
+
+         //   formGraphics.Clear(Color.White);
+
+            formGraphics.DrawString("Hello", drawFont, BlackBrush, 100, 400);
+
+
 
         }
 
-        
+
+
+
+
     }
 }
